d067572bbe96710490189ce682ad42991c841644
SoloSaravanan <SoloSaravanan@proton.me>
2026-09-26 06:29:10 +0000

    cpuidle: allow states at equal latency limits

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -664,7 +664,7 @@ static int cpu_power_select(struct cpuidle_device *dev,
 		min_residency = pwr_params->min_residency;
 		max_residency = pwr_params->max_residency;
 
-		if (latency_us <= lvl_latency_us)
+		if (latency_us < lvl_latency_us)
 			break;
 
 		if (next_event_us) {
